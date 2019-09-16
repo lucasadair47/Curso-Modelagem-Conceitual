f18556c7ee4882cd6765112a690596d64ae26c90
Lucas Adair <lucasadair47@gmail.com>
2019-09-16 12:21:21 -0300

    REST Testado

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -14,16 +14,9 @@ import com.nelioalves.cursomc.domain.Categoria;
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	public String listar() {
+		return "Rest está funcionando";
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
 	}
 
 }
